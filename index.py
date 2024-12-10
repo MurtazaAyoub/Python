4bf4b538454df401e8389110c2005af941489737
MurtazaAyoub <shuzee8@gmail.com>
2024-12-10 11:58:30 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd 
+
+# arr = np.array([1, 2, 3, 4, 5])
+# print(arr)
+
+
+# arr = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+# print(arr)
+# print(arr.sort())
+# print(arr.sum())
+
+# arr = np.array([1, 2, 3, 4])
+# print(arr[0])
+
+# arr = np.array([1, 2, 3, 4])
+# print(arr[2] + arr[3])
+
+# arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+# print(arr[0, 1, 2])
+
+# arr = np.array([1, 2, 3, 4, 5, 6, 7])
+# print(arr[1:5])
+
+# arr = np.array([1, 2, 3, 4, 5, 6, 7])
+# print(arr[-3:-1])
+
+# a = [1, 3, 2]
+# x = pd.Series(a)
+# print(x)
+
+# a = [1, 3, 2]
+# label = pd.Series(a, index = ["a", "b", "c"])
+# print(label)
+# print(label[0])
+# print(label["b"])
+
+
+#FILE HANDLING
+# f=open('new.txt','a')
+# f.write("what are you doing\n")
+# f.write("new line\n")
+# f.write("yesss\n")
+# f.close()
+
+
+# f=open('new.txt','r')
+# print(f.read())
+
+# f = open("myfile.txt", "a")
+# f.write("my name is Khan\n")
+# f.close()
+
+# f = open("myfile.txt", "r")
+# print(f.read())
